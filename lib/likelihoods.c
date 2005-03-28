16e6176cbc9dad7e883e08edced99683f6ffad9f
ben_k <ben_k@add2a0e0-a010-0410-b1a8-d2f0145315fb>
2005-03-28 00:47:31 +0000

    Cleaned up some errors in Waring estimator; apop_mle_xxx now returns beta, instead of asking it as a parameter.
    git-svn-id: https://apophenia.svn.sourceforge.net/svnroot/apophenia/trunk/apophenia@28 add2a0e0-a010-0410-b1a8-d2f0145315fb

--- lib/likelihoods.c
+++ lib/likelihoods.c
@@ -62,7 +62,7 @@ float 		llikelihood 	= 0,
 void d_gamma_likelihood(const gsl_vector *beta, void *d, gsl_vector *gradient){
 float		a	= gsl_vector_get(beta, 0),
 		b	= gsl_vector_get(beta, 1);
-	if (a <= 0 || b <= 0 || gsl_isnan(a) || gsl_isnan(b)) return GSL_POSINF;	
+	//if (a <= 0 || b <= 0 || gsl_isnan(a) || gsl_isnan(b)) return GSL_POSINF;	
 						//a sign to the minimizer to look elsewhere.
 int 		i, k;
 gsl_matrix	*data	= d;
@@ -88,9 +88,13 @@ void gamma_fdf(const gsl_vector *beta, void *d, double *f, gsl_vector *df){
 	d_gamma_likelihood(beta, d, df);
 }
 
-double apop_mle_gamma(gsl_matrix *data, gsl_vector **beta, double *starting_pt, double step_size, int verbose){
-	return maximum_likelihood_w_d(data, beta, 2, &apop_gamma_likelihood, d_gamma_likelihood, gamma_fdf, 
+gsl_vector * apop_mle_gamma(gsl_matrix *data, double *likelihood, double *starting_pt, double step_size, int verbose){
+gsl_vector	*beta;
+double		ll;
+	ll	= maximum_likelihood_w_d(data, &beta, 2, &apop_gamma_likelihood, d_gamma_likelihood, gamma_fdf, 
 							starting_pt, step_size, verbose);
+	if (likelihood != NULL)	*likelihood	= ll;
+	return beta;
 }
 
 
@@ -159,23 +163,29 @@ void probit_fdf( const gsl_vector *beta, void *d, double *f, gsl_vector *df){
 }
 
 
-double apop_mle_probit(gsl_matrix *data, gsl_vector **beta, double *starting_pt, double step_size, int verbose){
-	return	maximum_likelihood_w_d(data, beta, data->size2 - 1, 
-					apop_probit_likelihood, d_probit_likelihood, probit_fdf, 
-					starting_pt, step_size, verbose);
+gsl_vector * apop_mle_probit(gsl_matrix *data, double *likelihood, double *starting_pt, double step_size, int verbose){
+gsl_vector	*beta;
+double		ll;
+	ll	= maximum_likelihood_w_d(data, &beta, data->size2 - 1, apop_probit_likelihood, 
+			d_probit_likelihood, probit_fdf, starting_pt, step_size, verbose);
+	if (likelihood != NULL)	*likelihood	= ll;
+	return beta;
 }
-
 
 
 /////////////////////////
 //The Waring distribution
 /////////////////////////
+//W(x, b,a) 	= (b-1) g(b+a) g(k+a) / [g(a+1) g(k+a+b)], where g(x) =gamma(x)
+//ln W(x, b, a) = ln(b-1) + lng(b+a) + lng(k+a) - lng(a+1) - lng(k+a+b)
+//dlnW/db	= 1/(b-1)  + psi(b+a) - psi(k+a+b)
+//dlnW/da	= psi(b+a) + psi(k+a) - psi(a+1) - psi(k+a+b)
 
 
 double apop_waring_likelihood(const gsl_vector *beta, void *d){
 float		bb	= gsl_vector_get(beta, 0),
 		a	= gsl_vector_get(beta, 1);
-	if (bb <=2 || a <= 0) return GSL_POSINF;	//a sign to the minimizer to look elsewhere.
+	if (bb <=2 || a < 0) return GSL_POSINF;	//a sign to the minimizer to look elsewhere.
 int 		i, k;
 gsl_matrix*	data		= d;
 double 		ln_a_k, ln_bb_a_k,
@@ -183,12 +193,13 @@ double 		ln_a_k, ln_bb_a_k,
 		ln_bb_a		= gsl_sf_lngamma(bb + a),
 		ln_a_mas_1	= gsl_sf_lngamma(a + 1),
 		ln_bb_less_1	= log(bb - 1);
-	for (k=0; k< data->size2; k++)	//more efficient to go column-by-column
+	for (k=0; k< data->size2; k++){	//more efficient to go column-by-column
+		ln_bb_a_k	 = gsl_sf_lngamma(k  + a + bb);
+		ln_a_k		 = gsl_sf_lngamma(k  + a);
 		for (i=0; i< data->size1; i++){
-			ln_bb_a_k	 = gsl_sf_lngamma(k  + a + bb);
-			ln_a_k		 = gsl_sf_lngamma(k  + a);
 			likelihood	+= gsl_matrix_get(data, i, k) *(ln_bb_less_1 + ln_a_k + ln_bb_a - ln_a_mas_1 - ln_bb_a_k);
 		}
+	}
 	return -likelihood;
 }
 
@@ -200,19 +211,21 @@ int 		i, k;
 gsl_matrix	*data		= d;
 double		bb_minus_one_inv= 1/(bb-1),
 		psi_a_bb	= gsl_sf_psi(bb + a),
+		psi_a_mas_one	= gsl_sf_psi(a+1),
 		psi_a_k,
 		psi_bb_a_k,
 		d_bb		= 0,
 		d_a		= 0;
-	for (k=0; k< data->size2; k++)	//more efficient to go column-by-column
+	for (k=0; k< data->size2; k++){	//more efficient to go column-by-column
+		psi_bb_a_k	 = gsl_sf_psi(k  + a + bb);
+		psi_a_k		 = gsl_sf_psi(k  + a);
 		for (i=0; i< data->size1; i++){
-			psi_bb_a_k	 = gsl_sf_psi(k  + a + bb);
-			psi_a_k		 = gsl_sf_psi(k  + a);
-			d_a		+= gsl_matrix_get(data, i, k) *(psi_a_k + psi_a_k - psi_bb_a_k);
 			d_bb		+= gsl_matrix_get(data, i, k) *(bb_minus_one_inv + psi_a_bb - psi_bb_a_k);
+			d_a		+= gsl_matrix_get(data, i, k) *(psi_a_bb + psi_a_k - psi_a_mas_one - psi_bb_a_k);
 		}
-	gsl_vector_set(gradient, 0, -d_a);
-	gsl_vector_set(gradient, 1, -d_bb);
+	}
+	gsl_vector_set(gradient, 0, -d_bb);
+	gsl_vector_set(gradient, 1, -d_a);
 }
 
 void waring_fdf(const gsl_vector *beta, void *d, double *f, gsl_vector *df){
@@ -220,17 +233,30 @@ void waring_fdf(const gsl_vector *beta, void *d, double *f, gsl_vector *df){
 	d_waring_likelihood(beta, d, df);
 }
 
+/*
 double apop_mle_waring(gsl_matrix *data, gsl_vector **beta, double *starting_pt, double step_size, int verbose){
 	return maximum_likelihood_w_d(data, beta, 2, &apop_waring_likelihood, d_waring_likelihood, waring_fdf, 
 							starting_pt, step_size, verbose);
+}
+*/
+gsl_vector * apop_mle_waring(gsl_matrix *data, double *likelihood, double *starting_pt, double step_size, int verbose){
+gsl_vector	*beta;
+double		ll;
+	ll	=  maximum_likelihood_w_d(data, &beta, 2, &apop_waring_likelihood, d_waring_likelihood, waring_fdf, 
+							starting_pt, step_size, verbose);
+	if (likelihood != NULL)	*likelihood	= ll;
+	return beta;
 }
 
 
 ///////////////////////
 //The Yule distribution
 ///////////////////////
-
 //Yule likelihood fn. The special case of Waring where alpha = 0.
+//Y(x, b) 	= (b-1) g(b) g(k) / g(k+b), where g(x) =gamma(x)
+//ln Y(x, b) 	= ln(b-1) + lng(b) + lng(k) - lng(k+b)
+//dlnY/db	= 1/(b-1)  + psi(b) - psi(k+b)
+
 double apop_yule_likelihood(const gsl_vector *beta, void *d){
 float		bb		= gsl_vector_get(beta, 0);
 	if (bb <=2) return GSL_POSINF;	//a sign to the minimizer to look elsewhere.
@@ -240,15 +266,14 @@ float 		ln_k, ln_bb_k,
 	likelihood 	= 0,
 	ln_bb		= gsl_sf_lngamma(bb),
 	ln_bb_less_1	= log(bb-1);
-	for (k=0; k< data->size2; k++)	
+	for (k=0; k< data->size2; k++)	{
+		if (k>1) 	ln_k	= gsl_sf_lngamma(k);
+		else		ln_k	= 0;
+		ln_bb_k		= gsl_sf_lngamma(k+bb);
 		for (i=0; i< data->size1; i++){
-			//if (k>1) 	ln_k	= gsl_sf_lngamma(k+1);
-			if (k>1) 	ln_k	= gsl_sf_lngamma(k);
-			else		ln_k	= 0;
-			//ln_bb_k	= gsl_sf_lngamma(k+1+bb);
-			ln_bb_k		= gsl_sf_lngamma(k+bb);
 			likelihood	+= gsl_matrix_get(data,i,k) * (ln_bb_less_1 + ln_k + ln_bb - ln_bb_k);
 		}
+	}
 	return -likelihood;
 }
 
@@ -261,11 +286,12 @@ double		bb_minus_one_inv= 1/(bb-1),
 		psi_bb		= gsl_sf_psi(bb),
 		psi_bb_k,
 		d_bb		= 0;
-	for (k=0; k< data->size2; k++)	//more efficient to go column-by-column
+	for (k=0; k< data->size2; k++){
+		psi_bb_k	 = gsl_sf_psi(k + bb);
 		for (i=0; i< data->size1; i++){
-			psi_bb_k	 = gsl_sf_psi(k + bb);
 			d_bb		+= gsl_matrix_get(data, i, k) *(bb_minus_one_inv + psi_bb - psi_bb_k);
 		}
+	}
 	gsl_vector_set(gradient, 0, -d_bb);
 }
 
@@ -274,10 +300,22 @@ void yule_fdf(const gsl_vector *beta, void *d, double *f, gsl_vector *df){
 	d_yule_likelihood(beta, d, df);
 }
 
+gsl_vector * apop_mle_yule(gsl_matrix *data, double *likelihood, double *starting_pt, double step_size, int verbose){
+gsl_vector	*beta;
+double		ll;
+	ll	=  maximum_likelihood_w_d(data, &beta, 1, &apop_yule_likelihood, d_yule_likelihood, yule_fdf, 
+							starting_pt, step_size, verbose);
+	if (likelihood != NULL)	*likelihood	= ll;
+	return beta;
+}
+
+/*
 double apop_mle_yule(gsl_matrix *data, gsl_vector **beta, double *starting_pt, double step_size, int verbose){
+gsl_vector	**beta;
 	return maximum_likelihood_w_d(data, beta, 1, &apop_yule_likelihood, d_yule_likelihood, yule_fdf, 
 							starting_pt, step_size, verbose);
 }
+*/
 
 
 
@@ -322,9 +360,13 @@ void zipf_fdf(const gsl_vector *beta, void *d, double *f, gsl_vector *df){
 	d_zipf_likelihood(beta, d, df);
 }
 
-double apop_mle_zipf(gsl_matrix *data, gsl_vector **beta, double *starting_pt, double step_size, int verbose){
-	return maximum_likelihood_w_d(data, beta, 1, &apop_zipf_likelihood, d_zipf_likelihood, zipf_fdf, 
+gsl_vector * apop_mle_zipf(gsl_matrix *data, double *likelihood, double *starting_pt, double step_size, int verbose){
+gsl_vector	*beta;
+double		ll;
+	ll	= maximum_likelihood_w_d(data, &beta, 1, &apop_zipf_likelihood, d_zipf_likelihood, zipf_fdf, 
 							starting_pt, step_size, verbose);
+	if (likelihood != NULL)	*likelihood	= ll;
+	return beta;
 }
 
 
@@ -435,3 +477,16 @@ int				iter =0, status;
 	gsl_multimin_fdfminimizer_free(s);
 	return likelihood(*betas, data);
 }
+
+
+void apop_make_likelihood_vector(gsl_matrix *m, gsl_vector **v, 
+				double (*likelihood_fn)(const gsl_vector *beta, void *d), gsl_vector* fn_beta){
+gsl_matrix_view mm;
+int             i;
+	*v	= gsl_vector_alloc(m->size1);
+        for(i=0; i< m->size1; i++){
+                mm      = gsl_matrix_submatrix(m,i,0, 1,m->size2);      //get a single row
+                gsl_vector_set(*v, i, likelihood_fn(fn_beta, (void *) &(mm.matrix)));
+        }
+}
+
--- lib/likelihoods.h
+++ lib/likelihoods.h
@@ -12,18 +12,17 @@
 #define MAX_ITERATIONS 		500
 #define MAX_ITERATIONS_w_d	500
 
-double apop_mle_probit(gsl_matrix *data, gsl_vector **beta, double *starting_pt, double step_size, int verbose);
+gsl_vector * apop_mle_probit(gsl_matrix *data, double *likelihood, double *starting_pt, double step_size, int verbose);
 double apop_probit_likelihood(const gsl_vector *beta, void *d);
 
-double apop_mle_gamma(gsl_matrix *data, gsl_vector **beta, double *starting_pt, double step_size, int verbose);
-double apop_mle_waring(gsl_matrix *data, gsl_vector **beta, double *starting_pt, double step_size, int verbose);
-double apop_mle_yule(gsl_matrix *data, gsl_vector **beta, double *starting_pt, double step_size, int verbose);
-double apop_mle_zipf(gsl_matrix *data, gsl_vector **beta, double *starting_pt, double step_size, int verbose);
+gsl_vector * apop_mle_gamma(gsl_matrix *data, double *likelihood, double *starting_pt, double step_size, int verbose);
+gsl_vector * apop_mle_waring(gsl_matrix *data, double *likelihood, double *starting_pt, double step_size, int verbose);
+gsl_vector * apop_mle_yule(gsl_matrix *data, double *likelihood, double *starting_pt, double step_size, int verbose);
+gsl_vector * apop_mle_zipf(gsl_matrix *data, double *likelihood, double *starting_pt, double step_size, int verbose);
 double apop_gamma_likelihood(const gsl_vector *beta, void *d);
 double apop_waring_likelihood(const gsl_vector *beta, void *d);
 double apop_yule_likelihood(const gsl_vector *beta, void *d);
 double apop_zipf_likelihood(const gsl_vector *beta, void *d);
-
 /*
 For the Probit, the first column of the data matrix is the dependent
 variable, and the remaining variables are the independent. This means
@@ -33,9 +32,8 @@ For the Waring, Yule, and Zipf estimates, each row of the data matrix
 is one observation. The first column is the number of elements with one
 link, the second is the number of elements with two links, et cetera.
 
-Beta needs to be declared but should not be allocated; these functions
-do the appropriate allocation for you. When the function returns, Beta
-will have the most likely estimate.
+If you want the total likelihood, likelihood should be a double*; else,
+send in NULL and get nothing back.
 
 starting_pt is a vector of the appropriate size which indicates your
 best initial guess for beta. if starting_pt=NULL, then (0,0,...0) will
@@ -48,18 +46,26 @@ same step_size.
 verbose is zero or one depending on whether you want to see the
 maximizer's iterations.
 
-For each function, the return value is the log likelihood (i.e.,
-not the MLE, which is returned in beta, but the probability that the
-MLE is true).
+For each function, the return value is the vector of most likely parameters.
 
 Sample usage:
 
 gsl_vector * 	waring_parameter; 	//do not allocate.
 double 		starting_pt[2] = {3, 0};
 double		likelihood;
-likelihood	= mle_waring(data, &zipf_parameter, starting_pt, .01, 0);
+waring_parameter	= mle_waring(data, &likelihood, starting_pt, .01, 0);
 printf("Your most likely waring parameter is %g, with likelihood %g", 
 				gsl_vector_get(waring_parameter, 0), likelihood);
-gsl_vector_free waring_parameter; 	//Don't forget to clean up when you're done.
+gsl_vector_free(waring_parameter); 	//Don't forget to clean up when you're done.
 
 */
+
+void apop_make_likelihood_vector(gsl_matrix *m, gsl_vector **v, 
+				double (*likelihood_fn)(const gsl_vector *beta, void *d), gsl_vector* fn_beta);
+/*This function goes row by row through m and calculates the likelihood
+  of the given row, putting the result in v. You will need this to find
+  the variance of the estimator.
+  The likelihood function can be any of &apop_xxx_likelihood from above, 
+  and fn_beta will probably be the beta calculated using the corresponding
+  apop_xxx_mle function.
+  */
