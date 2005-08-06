45a37bfcfee27c6b2dd796dc03cef293d8f53bb8
ben_k <ben_k@add2a0e0-a010-0410-b1a8-d2f0145315fb>
2005-08-06 18:39:55 +0000

    flags and such.
    git-svn-id: https://apophenia.svn.sourceforge.net/svnroot/apophenia/trunk/apophenia@72 add2a0e0-a010-0410-b1a8-d2f0145315fb

--- lib/apop_merge_dbs.c
+++ lib/apop_merge_dbs.c
@@ -5,11 +5,9 @@
 int main(int argc, char **argv){
 char		c, 
 		*delimiter,
-		msg[1000],
-		*outfile	=NULL;
-gsl_matrix	*m;
-
-	sprintf(msg, "%s [opts] main_db.db db_to_merge_into_main.db\n", argv[0]); 
+		msg[1000];
+	sprintf(msg, "%s [opts] main_db.db db_to_merge_into_main.db\n\
+			-v\tverbose\n", argv[0]); 
 
 	if(argc<3){
 		printf(msg);
@@ -17,15 +15,18 @@ gsl_matrix	*m;
 	}
 	delimiter	= malloc(sizeof(char) * 5);
 	strcpy(delimiter, ",");
-	while ((c = getopt (argc, argv, "d:f:h")) != -1){
+	while ((c = getopt (argc, argv, "vh")) != -1){
 		switch (c){
+		  case 'v':
+			apop_verbose	++;
+			break;
 		  case 'h':
 			printf(msg);
 			return 0;
 		}
 	}
 	apop_open_db(argv[optind]);
-	m	= apop_merge_db(argv[optind +1]);
+	apop_db_merge(argv[optind +1]);
 	apop_close_db(0);
 	return 0;
 }
