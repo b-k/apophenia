ca79ec98bdd0f36d39f95ee724852cd075620ea4
ben_k <ben_k@add2a0e0-a010-0410-b1a8-d2f0145315fb>
2005-11-19 18:09:30 +0000

    I forgot to add output.h to repositiry.
    git-svn-id: https://apophenia.svn.sourceforge.net/svnroot/apophenia/trunk/apophenia@92 add2a0e0-a010-0410-b1a8-d2f0145315fb

---
+++ lib/output.h
@@ -0,0 +1,24 @@
+#include <gsl/gsl_matrix.h>
+#include <apophenia/db.h>
+#include <apophenia/name.h>
+#include <apophenia/stats.h>
+#include <apophenia/estimate.h>
+#include <apophenia/linear_algebra.h>
+
+
+void apop_plot_line_and_scatter(gsl_matrix *data, apop_estimate *est, apop_name *n, char *outfile);
+void apop_plot(gsl_matrix *data, char plot_type, int delay);
+void apop_plot_histogram(gsl_vector *data, size_t bin_ct, char *outfile);
+
+void apop_print_to_file(char *filename, const char *fmt, ...);
+void apop_print_matrix(gsl_matrix *data, char *separator, char* file);
+void apop_print_matrix_int(gsl_matrix *data, char *separator, char* file);
+void apop_print_vector(gsl_vector *data, char *separator, char* file);
+void apop_print_vector_int(gsl_vector *data, char *separator, char* file);
+
+void apop_matrix_print(gsl_matrix *data, char *separator, char* file);
+void apop_matrix_print_int(gsl_matrix *data, char *separator, char* file);
+void apop_vector_print(gsl_vector *data, char *separator, char* file);
+void apop_vector_print_int(gsl_vector *data, char *separator, char* file);
+
+gsl_matrix * apop_matrix_summarize(gsl_matrix *data, apop_name *names_in, apop_name **names_out); //void apop_matrix_summarize(gsl_matrix *data, apop_name *names);
