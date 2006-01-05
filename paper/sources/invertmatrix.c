fb0b586d53d59e5c1cb3d7aace4771e85b60836b
ben_k <ben_k@add2a0e0-a010-0410-b1a8-d2f0145315fb>
2006-01-05 22:28:51 +0000

    Book is increasingly coherent, up to about chapter 5.
    git-svn-id: https://apophenia.svn.sourceforge.net/svnroot/apophenia/trunk/apophenia@118 add2a0e0-a010-0410-b1a8-d2f0145315fb

--- paper/sources/invertmatrix.c
+++ paper/sources/invertmatrix.c
@@ -1,14 +1,20 @@
 #include <gsl/gsl_linalg.h>
 
-void function invert_matrix(gsl_matrix *in, gsl_matrix *out)
-{
+double apop_det_and_inv(gsl_matrix *in, gsl_matrix **out, int calc_det, int calc_inv) {
+int         sign;
+double      the_determinant = 0;
     gsl_matrix *invert_me = gsl_matrix_alloc(in->size1, in->size1);
     gsl_permutation * perm = gsl_permutation_alloc(in->size1);
-    int dummy;
     invert_me = gsl_matrix_alloc(in->size1, in->size1);
     gsl_matrix_memcpy (invert_me, in);
-    gsl_linalg_LU_decomp(invert_me, perm, &dummy);
-    gsl_linalg_LU_invert(invert_me, perm, out);
+    gsl_linalg_LU_decomp(invert_me, perm, &sign);
+    if (calc_inv){
+        *out    = gsl_matrix_alloc(in->size1, in->size1); //square.
+        gsl_linalg_LU_invert(invert_me, perm, *out);
+        }
+    if (calc_det)
+        the_determinant = gsl_linalg_LU_det(invert_me, sign);
     gsl_matrix_free(invert_me);
     gsl_permutation_free(perm);
+    return(the_determinant);
 }
