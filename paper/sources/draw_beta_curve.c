f96561ad620a5001368aa54d8995053d79ab2dec
ben_k <ben_k@add2a0e0-a010-0410-b1a8-d2f0145315fb>
2006-04-04 21:25:07 +0000

    The beta distribution plotting example for the textbook.
    git-svn-id: https://apophenia.svn.sourceforge.net/svnroot/apophenia/trunk/apophenia@144 add2a0e0-a010-0410-b1a8-d2f0145315fb

---
+++ paper/sources/draw_beta_curve.c
@@ -0,0 +1,17 @@
+#include <apophenia/headers.h>
+
+int main(){
+int				i;
+int				draws	= 1e6;
+int				bins	= 100;
+double		mu		= 0.492;    //also try 3./8.
+double		sigma	= 0.093;    //also try 1./24.
+char			outfile[]= "betaplot";
+gsl_rng			*r		= apop_rng_alloc(0);
+gsl_vector	*v		= gsl_vector_alloc(draws);
+    for (i =0; i < draws; i++)
+        gsl_vector_set(v, i, apop_random_beta(r, mu, sigma));
+    remove(outfile);
+    apop_plot_histogram(v, bins, outfile);
+    return 0;
+}
