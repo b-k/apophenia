4b7136f6ed150cfa73a54545f30069eac20a7170
ben_k <ben_k@add2a0e0-a010-0410-b1a8-d2f0145315fb>
2006-11-11 21:30:14 +0000

    removed textbook from repository. Asst bug fixes in lib.
    git-svn-id: https://apophenia.svn.sourceforge.net/svnroot/apophenia/trunk/apophenia@213 add2a0e0-a010-0410-b1a8-d2f0145315fb

--- paper/sources/env.c
+++
@@ -1,9 +0,0 @@
-#include <stdlib.h> //environment handling
-#include <stdio.h>
-
-int main(){
-    printf("You are: %s\n", getenv("USER"));
-    setenv("USER", "Steven", 1);
-    printf("But now you are: %s\n", getenv("USER"));
-    return 0;
-}
--- paper/sources/markov.c
+++
@@ -1,8 +0,0 @@
-#include <apophenia/headers.h>
-int main(){
-    apop_data *t = apop_text_to_data("data-markov", 0, 0);
-    gsl_matrix *out= gsl_matrix_calloc(t->matrix->size1,t->matrix->size1);
-    gsl_blas_dgemm(CblasNoTrans,CblasNoTrans, 1, t->matrix, t->matrix,0, out);
-    apop_matrix_show(out);
-    return 0;
-}
