1511e8b3929aadce0b42e94799b1fbdfe5a924b5
Ben <ben@klemens.org>
2014-09-06 13:59:37 -0400

    Benoit patch #4 & 5

--- cmd/apop_db_to_crosstab.c
+++ cmd/apop_db_to_crosstab.c
@@ -8,16 +8,17 @@ Command line utility to convert a three-column table to a crosstab.*/
 
 int main(int argc, char **argv){
     char c, verbose=0;
-    char const *msg="%s [opts] dbname table_name rows columns data\n\n"
-        "A command-line wrapper for the apop_db_to_crosstab function.\n"
-        "See Apophenia's online documentation for that function for details and tricks.\n"
-          "-d\tdelimiter\t\tdefault=<tab>\n"
-          "-v\tverbose: prints status info on stderr\n"
-          "-v -v\textra verbose: also print queries executed on stderr\n";
+    char const *msg="Usage: %s [opts] dbname table_name rows columns data\n"
+"\n"
+"A command-line wrapper for the apop_db_to_crosstab function.\n"
+"See Apophenia's online documentation for that function for details and tricks.\n"
+" -d\tdelimiter (default: <tab>)\n"
+" -v\tverbose: prints status info on stderr\n"
+" -v -v\tvery verbose: also print queries executed on stderr\n"
+" -h\tdisplay this help and exit\n"
+"\n";
 
-	Apop_stopif(argc<5, return 1, 0, msg, argv[0]);
-
-    apop_opts.verbose=0;  //so don't print queries until -v -v.
+	apop_opts.verbose=0;  //so don't print queries until -v -v.
 
 	while ((c = getopt (argc, argv, "d:f:hv-")) != -1)
 		if      (c=='d') strcpy(apop_opts.output_delimiter,optarg);
@@ -33,6 +34,6 @@ int main(int argc, char **argv){
             argv[optind], argv[optind +1], argv[optind+2], argv[optind+3], argv[optind+4]);
     }
 	apop_db_open(argv[optind]);
-    apop_data *m = apop_db_to_crosstab(argv[optind +1], argv[optind+2], argv[optind+3], argv[optind+4]);
+	apop_data *m = apop_db_to_crosstab(argv[optind +1], argv[optind+2], argv[optind+3], argv[optind+4]);
 	apop_data_print(m);
 }
